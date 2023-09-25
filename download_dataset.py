c45ba1038401953145439528a7f6a3367a565aea
aryanboloori97 <arianbolori@gmail.com>
2023-09-25 14:09:24 +0330

    download_dataset.py modified and refactored dramitically.

--- download_dataset.py
+++ download_dataset.py
@@ -1,29 +1,46 @@
+from pathlib import Path
+import requests
+from tqdm import tqdm
+import zipfile
+import os
 
-# Download and locate the dataset
-data_path = Path('./Data/')
-image_path = data_path / 'pizza_stake_suchi'
+class DownloadFile(object):
 
+    def __init__(self):
+        pass
+        
+       
+    @staticmethod
+    def download(url, file_name, folder_name='Data'):
+        # Download and locate the file
+        folder_path = Path(f'./{folder_name}/')
+        file_path = folder_path / file_name
 
-if image_path.is_dir():
-    print(f'Folder {image_path} already exists!')
-else:
-    print('Data folder Not found.... Creating one')
-    image_path.mkdir(parents=True, exist_ok=True)
+        if folder_path.is_dir():
+            print(f'Folder {folder_path} already exists!')
+        else:
+            print('Folder Not found.... Creating one')
+            folder_path.mkdir(parents=True, exist_ok=True)
 
-if not (image_path).exists():
-    with open(image_path/ 'pizza_stake_sushi.zip', 'wb') as f:
-        print('Downloading the dataset...')
-        req = requests.get('https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/data/pizza_steak_sushi.zip')
-        total_size_in_bytes= int(req.headers.get('content-length', 0))
-        block_size = 1024 #1 Kibibyte
-        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-        for data in req.iter_content(block_size):
-            progress_bar.update(len(data))
-            f.write(data)
-    progress_bar.close()
-    print('Dwonloading zip file finished....Starting uzipping it....!')
-    with zipfile.ZipFile(image_path / 'pizza_stake_sushi.zip', 'r') as f_2:
-        f_2.extractall(image_path)
-    os.remove(image_path / 'pizza_stake_sushi.zip')
-else:
-    print("Dataset : 'pizza_stake_sushi' Aready exists!")
+        if not (file_path).exists():
+            with open(folder_path/ file_path, 'wb') as f:
+                print('Downloading the file...')
+                req = requests.get('https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/data/pizza_steak_sushi.zip')
+                total_size_in_bytes= int(req.headers.get('content-length', 0))
+                block_size = 1024 #1 Kibibyte
+                progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+                for data in req.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    f.write(data)
+            progress_bar.close()
+        else:
+            print("Dataset : 'pizza_stake_sushi' Aready exists!")
+            
+            
+    @staticmethod
+    def unzip(file_path, keep_zip=True):
+            with zipfile.ZipFile(file_path, 'r') as f_2:
+                f_2.extractall(file_path.parent)
+            if not keep_zip:
+                os.remove(file_path)
+        
